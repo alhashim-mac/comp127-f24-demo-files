3b6110b8cc85378041b9ecdf7d5f2ea8044d1fa8
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2024-09-05 22:51:40 -0500

    Add 9/6 demo files

---
+++ src/c0906_ast/Assignment.java
@@ -0,0 +1,11 @@
+package c0906_ast;
+
+public class Assignment {
+    public static void main(String[] args) {
+        String Joshua = "Coffee";
+        String Coffee = "Arabica Beans";
+
+        System.out.println(Joshua);
+        System.out.println(Coffee);
+    }
+}
---
+++ src/c0906_ast/Puzzle.java
@@ -0,0 +1,9 @@
+package c0906_ast;
+
+public class Puzzle {
+    public static void main(String[] args) {
+        if (1 > 2)
+            System.out.println("Roses are red, violets are blue");
+            System.out.println("If you ask me, one is greater than two");
+    }
+}
