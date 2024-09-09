4c2bbf500c8bbf74a1e6f89ea90e01aff308786f
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2024-09-09 00:17:59 -0500

    Add 9/6 demo files

---
+++ src/c0909_method_calls/TrickyPrimitives.java
@@ -0,0 +1,13 @@
+package c0909_method_calls;
+
+public class TrickyPrimitives {
+    public static void main(String[] args) {
+        System.out.println('a' + 1);
+
+        int b = 'a';
+        System.out.println(b);
+
+        // char b2 = b;
+        // System.out.println(b2);
+    }
+}
