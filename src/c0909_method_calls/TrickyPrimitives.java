1368d442ca28882ed0de779ece12079ccd1774dd
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2024-09-11 09:52:58 -0500

    Update TrickyPrimitives.java

--- src/c0909_method_calls/TrickyPrimitives.java
+++ src/c0909_method_calls/TrickyPrimitives.java
@@ -2,10 +2,13 @@ package c0909_method_calls;
 
 public class TrickyPrimitives {
     public static void main(String[] args) {
-        System.out.println('a' + 1);
+        // System.out.println('a' + 1);
 
-        int b = 'a';
-        System.out.println(b);
+        // int b = 'a';
+        // System.out.println(b);
+
+        char c = 'a' +1;
+        System.out.println(c);
 
         // char b2 = b;
         // System.out.println(b2);
