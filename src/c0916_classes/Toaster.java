3ed16680c70170de306062549f0621add67a6f2c
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2024-09-16 13:00:22 -0500

    Add 9/16 class demo files

--- src/c0916_classes/Toaster.java
+++ src/c0916_classes/Toaster.java
@@ -1,5 +1,36 @@
 package c0916_classes;
 
 public class Toaster {
-    
+    private boolean operationStaus;
+    private String color;
+    private int currentTemp;
+
+    public Toaster(boolean os, String c, int ct) {
+        operationStaus = os;
+        color = c;
+        setCurrentTemp(ct);
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public boolean getOperationStatus() {
+        return operationStaus;
+    }
+
+    public void setCurrentTemp(int newTemp) {
+        if(newTemp > 0 && newTemp < 100) {
+            currentTemp = newTemp;
+        }
+    }
+
+    public int getCurrentTemp() {
+        return currentTemp;
+    }
+
+    @Override
+    public String toString() {
+        return operationStaus + " " + color + " " + currentTemp;
+    }
 }
--- src/c0916_classes/TosterDriver.java
+++ src/c0916_classes/TosterDriver.java
@@ -1,5 +1,19 @@
 package c0916_classes;
 
 public class TosterDriver {
-    
+    public static void main(String[] args) {
+        Toaster t1 = new Toaster(true, "red", 100);
+        
+        // t1.operationStaus = true;
+        // t1.color = "red";
+        // t1.setCurrentTemp(300);
+        // System.out.println(t1.getOperationStatus());
+        // System.out.println(t1.getColor());
+        // System.out.println(t1.getCurrentTemp());
+        
+        System.out.println(t1);
+        
+        Toaster t2 = new Toaster(false, "orage", 60);
+        System.out.println(t2);
+    }
 }
