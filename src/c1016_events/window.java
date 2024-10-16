62ac4a019728d5bd34a01b2898b7e3598a4e1185
Amin Alhashim <aalhashi@macalester.edu>
2024-10-16 09:29:16 -0500

    Update window.java
    Add other implementation approaches

--- src/c1016_events/window.java
+++ src/c1016_events/window.java
@@ -1,14 +1,52 @@
 package c1016_events;
 
 import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.events.MouseButtonEvent;
+import edu.macalester.graphics.events.MouseButtonEventHandler;
 
 public class window {
     public static void main(String[] args) {
         CanvasWindow canvas = new CanvasWindow("Event Demo", 500, 500);
-        canvas.onCharacterTyped(t -> System.out.println("Key pressed: " + t));
+        
+        /*
+         * void onMouseDown(MouseButtonEventHandler handler)
+         * Adds a listener that will receive an event when the mouse button goes down inside the window.
+         * 
+         * What is MouseButtonEventHandler?
+         * What is the lamda expression implementing?
+         */
+
+        /* Lamda expression with syntactic sugar */
         canvas.onMouseDown(e -> System.out.println("Mouse down @ " + e.getPosition()));
-        canvas.onMouseUp(e -> System.out.println("Mouse up @ " + e.getPosition()));
-        canvas.onMouseMove(e -> System.out.println("Mouse move @ " + e.getPosition()));
-        canvas.onDrag(e -> System.out.println("Mouse drag @ " + e.getPosition()));
+        
+        /* Lamda expression without syntactic sugar */
+        // canvas.onMouseDown((MouseButtonEvent e) -> {
+        //     System.out.println("Mouse down @ " + e.getPosition());
+        // });
+
+        /* Ananynimous class */
+        // canvas.onMouseDown(new MouseButtonEventHandler() {
+        //     public void handleEvent (MouseButtonEvent e) {
+        //         System.out.println("Mouse down @ " + e.getPosition());
+        //     };
+        // });
+
+        /* Inner class */
+        // class MBEHClass implements MouseButtonEventHandler {
+        //     @Override
+        //     public void handleEvent(MouseButtonEvent e) {
+        //         System.out.println("Mouse down @ " + e.getPosition());
+        //     }
+        // }
+        // canvas.onMouseDown(new MBEHClass());
+        
+        
+        /*
+         * Similar methods
+         */
+        // canvas.onMouseUp(e -> System.out.println("Mouse up @ " + e.getPosition()));
+        // canvas.onMouseMove(e -> System.out.println("Mouse move @ " + e.getPosition()));
+        // canvas.onDrag(e -> System.out.println("Mouse drag @ " + e.getPosition()));
+        // canvas.onCharacterTyped(t -> System.out.println("Key pressed: " + t));
     }
 }
