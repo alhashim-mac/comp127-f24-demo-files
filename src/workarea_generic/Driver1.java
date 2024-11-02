0d0571f466580c61b0cfb5ebba14c8a1a3b4f8c8
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2024-11-02 17:38:46 -0500

    Remove exam test files

--- src/workarea_generic/Driver1.java
+++
@@ -1,9 +0,0 @@
-package workarea_generic;
-
-public class Driver1 {
-    public static void main(String[] args) {
-        CoffeeBean cb = new CoffeeBean(10);
-        DriverHelper.applyDiscount(cb.getPrice(), .5);
-        System.out.println(cb.getPrice());
-    }
-}
